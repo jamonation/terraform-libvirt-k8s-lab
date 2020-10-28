0a088837f669f62bb8286f259bb02b3092d46313
Jamon Camisso <jamonation@gmail.com>
2020-10-28 09:59:34 -0400

    Split out playbook and roles for better bootstrapping

---
+++ ansible/roles/cluster-reset/files/init_script.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+kubeadm init --control-plane-endpoint "10.17.3.254:8080" --upload-certs --ignore-preflight-errors=DirAvailable--etc-kubernetes-manifests | grep -B2 '\-\-control-plane'
