60a5dacb52d5d032abb0e6b8f82e0b2fcfaaebea
Jamon Camisso <jamonation@gmail.com>
2020-10-29 21:09:41 -0400

    Add reset role; refactored cluster join tasks

--- ansible/roles/control-bootstrap/files/init_script.sh
+++ ansible/roles/control-bootstrap/files/init_script.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-kubeadm init --control-plane-endpoint "10.17.3.254:8080" --upload-certs --ignore-preflight-errors=DirAvailable--etc-kubernetes-manifests | grep -B2 '\-\-control-plane'
+kubeadm init --control-plane-endpoint '10.17.3.254:8080' --upload-certs --ignore-preflight-errors=DirAvailable--etc-kubernetes-manifests | grep -B2 '\-\-control-plane'
+
+#printf "$BOOTSTRAP_CMD --ignore-preflight-errors=DirAvailable--etc-kubernetes-manifests"
